54bc92fc782ef05c1f26da8d0ad5e4ef720c82f9
jibola <pro.ajibolaojo@gmail.com>
2022-10-19 00:19:33 +0100

    secret key

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 app_config = {
     'SQLALCHEMY_DATABASE_URI': os.environ.get("DATABASE_URL","sqlite:///app.db").replace("postgres://", "postgresql://"),
+    'SECRET_KEY': os.environ.get("SECRET_KEY","SECRET_KEY"),
     'DEBUG': True,
 }
